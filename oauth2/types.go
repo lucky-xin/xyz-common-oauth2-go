3bf16ac31ccef1eac1e1ae3c7ccc3d5722274fc3
luchaoxin <chaoxin.lu@pistonint.com>
2024-03-28 17:32:53 +0800

    添加注释，添加token 字段

--- oauth2/types.go
+++ oauth2/types.go
@@ -19,14 +19,25 @@ type TokenType string
 var (
 	OAUTH2 TokenType = "OAuth2"
 	SIGN   TokenType = "Signature"
-	INTRO  TokenType = "INTRO"
 )
 
+// Token 信息
 type Token struct {
-	Type   TokenType              `json:"type" binding:"required"`
-	Value  string                 `json:"value" binding:"required"`
+	// Type Token类型
+	Type TokenType `json:"type" binding:"required"`
+	// Token值
+	Value string `json:"value" binding:"required"`
+	// 租户id
+	Tid int64 `json:"tid" binding:"tid"`
+	// 用户id
+	Uid int64 `json:"uid" binding:"required"`
+	// 用户名称
+	Uname string `json:"uname" binding:"uname"`
+	// 扩展参数
 	Params map[string]interface{} `json:"params" binding:"required"`
 }
+
+// XyzClaims 自定义JWT claims
 type XyzClaims struct {
 	jwt.RegisteredClaims
 	Username string `json:"username" binding:"required"`
@@ -34,20 +45,29 @@ type XyzClaims struct {
 	UserId   int64  `json:"id" binding:"required"`
 }
 
+// KeyInf JWT解析key
 type KeyInf struct {
 	Id  string `json:"id" binding:"required"`
 	Key string `json:"key" binding:"required"`
 	Alg string `json:"alg" binding:"required"`
 }
 
+// EncryptionInf AES加密配置信息
 type EncryptionInf struct {
-	AppId     string `json:"appId" binding:"required"`
+	// APP id
+	AppId string `json:"appId" binding:"required"`
+	// APP Secret
 	AppSecret string `json:"appSecret" binding:"required"`
-	AESKey    string `json:"aesKey" binding:"required"`
-	AESIv     string `json:"aesIv" binding:"required"`
-	TenantId  int32  `json:"tenantId" binding:"required"`
-	Username  string `json:"username" binding:"required"`
-	UserId    int64  `json:"userId" binding:"required"`
+	// 获取Token key时，AES加密key
+	AESKey string `json:"aesKey" binding:"required"`
+	// 获取Token key时，AES加密iv
+	AESIv string `json:"aesIv" binding:"required"`
+	// 租户id
+	TenantId int32 `json:"tenantId" binding:"required"`
+	// 用户名称
+	Username string `json:"username" binding:"required"`
+	// 用户id
+	UserId int64 `json:"userId" binding:"required"`
 }
 
 var (
